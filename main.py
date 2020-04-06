0f903f06d6d3d5d30e007112fde95d5319bdaeeb
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2020-04-06 12:41:11 -0300

    Atualização da main com a classe Grafo
    Seguem testes de funcionamento dos métodos da classe. Saída esperada:
    defaultdict(<class 'dict'>, {'A': ['1', '2', '3'], 'B': ['3', '4', '5', '6'], 'C': ['6', '7', '8']})
    defaultdict(<class 'dict'>, {'A': ['1', '2', '3'], 'B': ['3', '4', '5', '6'], 'C': ['6', '7', '8'], 'D': [7]})   
    defaultdict(<class 'dict'>, {})

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-from leitor_de_entrada import *
+from leitor_de_entrada import le_entrada
+from grafo_lib import Grafo
 
-print(le_entrada())
+g = Grafo(le_entrada())
+
+print(g.grafo)
+
+g.adiciona_vertice('D')
+
+g.adiciona_aresta_direcionada('D',5)
+g.adiciona_aresta_direcionada('D',7)
+g.elimina_aresta('D',5)
+
+print(g.grafo)
+
+g.elimina_vertice('A')
+g.elimina_vertice('B')
+g.elimina_vertice('C')
+g.elimina_vertice('D')
+
+print(g.grafo)
