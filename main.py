bd72def056de5670f08fde9648325c5cb7e8e559
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2020-04-07 22:03:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,6 @@ g = Grafo(le_entrada())
 
 print(g.grafo)
 
-g.adiciona_vertice('D')
-
-g.adiciona_aresta_direcionada('D',5)
-g.adiciona_aresta_direcionada('D',7)
-g.elimina_aresta('D',5)
-
-print(g.grafo)
-
-g.elimina_vertice('A')
-g.elimina_vertice('B')
-g.elimina_vertice('C')
-g.elimina_vertice('D')
+g.elimina_vertice('V3')
 
 print(g.grafo)
