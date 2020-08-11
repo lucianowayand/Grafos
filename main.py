e9f350f7d90d86c7fac5083cad3c61c660b9081b
Luciano <lucianowayand@hotmail.com>
2020-08-11 00:17:29 -0300

    Comentarios e correções

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
 from leitor_de_entrada import le_entrada
 from grafo_lib import Grafo
+from tkinter import *
+from PIL import Image, ImageTk
+   
+def destroy(): #Mata a primeira tela e armazena o valor da entry caso ela seja válida
+    try:
+        global num_vertices
+        num_vertices = int(vertices.get())
+        seletor.destroy()
+    except:
+        print("Valor inválido")
+
+#Configura a janela para aparecer no centro e não ser re-escalavel
+seletor = Tk()
+seletor.resizable(height=FALSE,width=FALSE)
+seletor.geometry("400x250")
+seletor.eval('tk::PlaceWindow %s center' % seletor.winfo_pathname(seletor.winfo_id()))
+seletor.title("Seletor de tamanho")
+
+#Insere os widgets
+info = Label(text="Insira o número de vértices")
+info.place(x=110,y=50)
+
+vertices = Entry()
+vertices.place(x=110,y=100)
+
+ok = Button(command=destroy,text="Criar")
+ok.place(x=160,y=140)
+
+disclaimer = Label(text="Esse processo pode demorar um pouco")
+disclaimer.place(x=75,y=200)
+
+#Fim do seletor
+seletor.mainloop()
 
 
+
+#Janela de exposição de resultado
+grafo = Tk()
+grafo.title("Grafo resultante de BFS")
+grafo.resizable(height=FALSE,width=FALSE)
+
+#Cria um grafo aleatorio e o categoriza de acordo com o BFS
 g = Grafo()
-print('\nESCOLHA UMA DAS OPCOES:')
-print('GRAFO DE:')
-print('1- 50 vertices')
-print('2- 100 vertices')
-print('3- 200 vertices')
-print('4- 300 vertices')
-entrada = int(input())
-if(entrada == 1):
-    g.cria_grafo_aleatorio(50)
-elif(entrada == 2):
-    g.cria_grafo_aleatorio(100)
-elif(entrada == 3):
-    g.cria_grafo_aleatorio(200)
-elif(entrada == 4):
-    g.cria_grafo_aleatorio(300)
-print("Criando grafo, espere por favor...")
+g.cria_grafo_aleatorio(num_vertices)
+g.iniciarBFS()
 g.cria_graphviz()
-print("\nGRAFO CRIADO COM SUCESSO!\n")
-print('\nPressione "0" para mostrar o BFS...')
-if int(input()) == 0:
-    g.iniciarBFS()
-    g.cria_graphviz()
+
+#Coloca a imagem gerada em uma janela da biblioteca tkinter
+img = ImageTk.PhotoImage(Image.open("test-output/grafo.png").resize((800,800)))
+panel = Label(grafo, image = img)
+panel.pack()
+
+grafo.mainloop()
